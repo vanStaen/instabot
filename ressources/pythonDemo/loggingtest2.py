9a51da814278ded08358c3b965890aa1a4f4d497
Clément van Staen <clementvanstaen@Clements-iMac.local>
2020-10-03 17:26:10 +0200

    test logger json failing

---
+++ ressources/pythonDemo/loggingtest2.py
@@ -0,0 +1,46 @@
+import logging.config
+import structlog
+
+from structlog import configure, processors, stdlib, threadlocal
+
+logging.config.dictConfig({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'json': {
+            'format': '%(message)s %(lineno)d %(pathname)s',
+            'class': 'pythonjsonlogger.jsonlogger.JsonFormatter'
+        }
+    },
+    'handlers': {
+        'json': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'json'
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['json'],
+            'level': logging.INFO
+        }
+    }
+})
+
+configure(
+    context_class=threadlocal.wrap_dict(dict),
+    logger_factory=stdlib.LoggerFactory(),
+    wrapper_class=stdlib.BoundLogger,
+    processors=[
+        stdlib.filter_by_level,
+        stdlib.add_logger_name,
+        stdlib.add_log_level,
+        stdlib.PositionalArgumentsFormatter(),
+        processors.TimeStamper(fmt="iso"),
+        processors.StackInfoRenderer(),
+        processors.format_exc_info,
+        processors.UnicodeDecoder(),
+        stdlib.render_to_log_kwargs]
+
+
+log = structlog.getLogger(__name__)
+log.info("event", some_param=42)
