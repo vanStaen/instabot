f348c2d2179b4f8271c17dcf69951b50345f88b7
Clément van Staen <clement.vanstaen@gmail.com>
2020-10-09 11:38:35 +0200

    postgres connection

--- ressources/pythonDemo/postgresTest.py
+++ ressources/pythonDemo/postgresTest.py
@@ -26,8 +26,10 @@ def connect():
 
         # close the communication with the PostgreSQL
         cur.close()
+
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
+
     finally:
         if conn is not None:
             conn.close()
