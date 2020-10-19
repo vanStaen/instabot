4df4ed9c0bff4e51f5bf0fb7670d436d4a7ac98a
Clément van Staen <clement.vanstaen@gmail.com>
2020-10-19 14:21:52 +0200

    crawler

--- ressources/instagramFollowersCrawler.py
+++ ressources/instagramFollowersCrawler.py
@@ -1,8 +1,9 @@
 # Get instance
+# if needed : sudo pip3 install instaloader
 import instaloader
 L = instaloader.Instaloader()
 
-user = "touwe"
+user = "real_missbanana1"
 
 # Login or load session
 L.login("clementvanstaen", "kftv2h4insta")
@@ -11,7 +12,7 @@ L.login("clementvanstaen", "kftv2h4insta")
 profile = instaloader.Profile.from_username(L.context, user)
 
 # Save list of followers
-file = open("insta_followers_of/"+ user +".txt","a+")
+file = open("insta_followers_of/" + user + ".txt", "a+")
 for follower in profile.get_followers():
     username = follower.username
     file.write(username + "\n")
