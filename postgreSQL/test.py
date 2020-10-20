671af5660dfc0835924e47d08204e79233aa415b
Clément van Staen <clement.vanstaen@gmail.com>
2020-10-20 17:26:15 +0200

    Reading account data from Heroku

--- postgreSQL/test.py
+++ postgreSQL/test.py
@@ -1,30 +1,57 @@
-from postgreSQL.fetch import fetchFirst
-from postgreSQL.delete import deleteUser
-import logging
+import psycopg2
+from configparser import ConfigParser
 
-# Move Test.py in the main folder to run it.
 
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-file_handler = logging.FileHandler('log/insta_bot.log')
-file_formatter = logging.Formatter(
-    "{'time':'%(asctime)s', 'level': '%(levelname)s', 'message': '%(message)s'}"
-)
-file_handler.setFormatter(file_formatter)
-logger.addHandler(file_handler)
+def configDB(section, filename='../database.ini'):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
 
-account = 'test'
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(
+            section, filename))
 
-result = fetchFirst(account)
-logging.info('Fetch user {} from postgreSQL table {}.'.format(result, account))
-print('Username: {}'.format(result))
+    return db
 
-deleteUser(account, result)
-print('Deleted: {}'.format(result))
 
-result = fetchFirst(account)
-logging.info('Fetch user {} from postgreSQL table {}.'.format(result, account))
-print('Username: {}'.format(result))
+def fetchAllAccount():
 
-deleteUser(account, result)
-print('Deleted: {}'.format(result))
+    try:
+        params = configDB(section='postgresql_heroku')
+        #print('Connecting to the PostgreSQL database...')
+        connection = psycopg2.connect(**params)
+
+        cursor = connection.cursor()
+        postgreSQL_select_Query = "SELECT * FROM public.config_accounts_insta;"
+
+        cursor.execute(postgreSQL_select_Query)
+        data = cursor.fetchall()
+
+        return data
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while fetching data from PostgreSQL", error)
+
+    finally:
+        # closing database connection.
+        if (connection):
+            cursor.close()
+            connection.close()
+            #print("PostgreSQL connection is closed")
+
+
+result = fetchAllAccount()
+#print('Username: {}'.format(result))
+for account in result:
+    print('###################')
+    print(account[0])
+    print(account[1])
+    print(account[2][0])
+    print(account[3])
