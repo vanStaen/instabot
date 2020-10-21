e94e4391d0ec475a8349ac85ba7561704bc0f709
Clément van Staen <clement.vanstaen@gmail.com>
2020-10-21 13:49:52 +0200

    refactoring of the fetch function

--- postgreSQL/fetch.py
+++ postgreSQL/fetch.py
@@ -21,8 +21,29 @@ def fetchFirst(table):
         print("Error while fetching data from PostgreSQL", error)
 
     finally:
-        # closing database connection.
         if (connection):
             cursor.close()
             connection.close()
-            #print("PostgreSQL connection is closed")
+
+
+def fetchAllAccount():
+
+    try:
+        params = configDB(section='postgresql_heroku')
+        connection = psycopg2.connect(**params)
+
+        cursor = connection.cursor()
+        postgreSQL_select_Query = "SELECT * FROM public.config_accounts_insta;"
+
+        cursor.execute(postgreSQL_select_Query)
+        data = cursor.fetchall()
+
+        return data
+
+    except (Exception, psycopg2.Error) as error:
+        print("Error while fetching data from PostgreSQL", error)
+
+    finally:
+        if (connection):
+            cursor.close()
+            connection.close()
