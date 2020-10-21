37b663497474b730f09ce12283ad893fc5a65cd4
Clément van Staen <clement.vanstaen@gmail.com>
2020-10-21 16:23:51 +0200

    Add landing page and express node.js index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const express = require("express");
+const PORT = process.env.PORT || 8080;
+
+// Init Express
+const app = express();
+
+// Set Static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Static pointing to the logs
+app.get("/log", (req, res) => {
+  res.sendFile(path.join(__dirname, "log/insta_bot.log"));
+});
+
+// Listen on a port
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
