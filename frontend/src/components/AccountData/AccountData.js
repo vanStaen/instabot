38fb2f450c8a2cd958d9b37cceada682516adcf4
vanStaen <clement.vanstaen@gmail.com>
2021-07-23 17:01:17 +0200

    show data accounts

--- frontend/src/components/AccountData/AccountData.js
+++ frontend/src/components/AccountData/AccountData.js
@@ -14,34 +14,6 @@ export const AccountData = () => {
   const fetchAccountData = async () => {
     try {
       const fetchedData = await getAccountData();
-      /* const fetchedDataWoEmoji = fetchedData.map((userData, index) => {
-        const userDataWoEmoji = {
-          ...userData,
-          key: `key_${index}`,
-          username:
-            userData.username &&
-            userData.username.replace(
-              /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
-              ""
-            ),
-          first_name:
-            userData.first_name &&
-            userData.first_name.replace(
-              /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
-              ""
-            ),
-          last_name:
-            userData.last_name &&
-            userData.last_name.replace(
-              /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
-              ""
-            ),
-        };
-        return userDataWoEmoji;
-      }); */
-      /* const fetchedDataWoEmojiFiltered = fetchedDataWoEmoji.filter((user) => {
-        return user.first_name !== 'Group' && user.first_name !== 'Telegram'
-      }); */
       setAccountData(fetchedData);
     } catch (err) {
       console.log(err);
@@ -71,6 +43,29 @@ export const AccountData = () => {
         }
         return a.username.localeCompare(b.username);
       },
+    },
+    {
+      title: "Iterations",
+      dataIndex: "iterations",
+      key: "iterations",
+      sorter: (a, b) => a.id - b.id,
+    },
+    {
+      title: "Tags",
+      dataIndex: "tags",
+      key: "tags",
+    },
+    {
+      title: "Active",
+      dataIndex: "active",
+      key: "active",
+      //sorter: (a, b) => a.id - b.id,
+    },
+    {
+      title: "Alive",
+      dataIndex: "alive",
+      key: "alive",
+      //sorter: (a, b) => a.id - b.id,
     },
   ];
 
