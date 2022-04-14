58e995a2dcb6d6dae4ed8b561957f331afd8135e
vanStaen <clement.vanstaen@gmail.com>
2022-04-14 10:02:56 +0200

    Error mail is clearer

--- helpers/sendMail.py
+++ helpers/sendMail.py
@@ -14,21 +14,29 @@ def sendMail(mailType, detail, iteration, runTime):
         messageBody = f"Instabot ran successfully with {iteration} iterations.<br/> Runtime of the script : {runTime}<br/>"
         messageBody = messageBody + "<ol>"
         for data in detail:
-            if detail[data]['connectionError']:
-                messageBody = messageBody + \
-                    f"<li>Error on connection with account <b>'{detail[data]['name']}'</b>.</li>"
-            elif not detail[data]['active']:
-                messageBody = messageBody + \
-                    f"<li>Account <b>{detail[data]['name']}</b><ul><li>is deactivated.</li></ul></li>"
-            elif not detail[data]['run']:
-                messageBody = messageBody + \
-                    f"<li>Account <b>{detail[data]['name']}</b><ul><li>did not run.</li></ul></li>"
+            if detail[data]["connectionError"]:
+                messageBody = (
+                    messageBody
+                    + f"<li>Error on connection with account <b>'{detail[data]['name']}'</b>.</li>"
+                )
+            elif not detail[data]["active"]:
+                messageBody = (
+                    messageBody
+                    + f"<li>Account <b>{detail[data]['name']}</b><ul><li>is deactivated.</li></ul></li>"
+                )
+            elif not detail[data]["run"]:
+                messageBody = (
+                    messageBody
+                    + f"<li>Account <b>{detail[data]['name']}</b><ul><li>did not run.</li></ul></li>"
+                )
             else:
-                messageBody = messageBody + \
-                    f"<li>Account <b>{detail[data]['name']}</b><ul><li>ran {detail[data]['iterations']} iterations of {detail[data]['iterationMax']} max.</li><li>generated {detail[data]['errors']} errors.</li><li>{detail[data]['databaseUser']} username remaining.</li></ul></li>"
+                messageBody = (
+                    messageBody
+                    + f"<li>Account <b>{detail[data]['name']}</b><ul><li>ran {detail[data]['iterations']} iterations of {detail[data]['iterationMax']} max.</li><li>generated {detail[data]['errors']} errors.</li><li>{detail[data]['databaseUser']} username remaining.</li></ul></li>"
+                )
         messageBody = messageBody + "</ol>"
     elif mailType == 1:
-        messageSubject = "Python Error report."
+        messageSubject = "Account '{detail}' is now deactivated"
         messageBody = f"There were too many errors when running the instabot script for the account <b>{detail}</b>. The account will be deactivated."
     elif mailType == 2:
         messageSubject = f"{formattedDateStamp}, Instabot script started."
@@ -38,12 +46,16 @@ def sendMail(mailType, detail, iteration, runTime):
         messageBody = f"The Weekly account update was <b>successful</b>.<br/> Run time of the script : {runTime}.<br/>"
         messageBody = messageBody + "<ol>"
         for data in detail:
-            if detail[data]['alive']:
-                messageBody = messageBody + \
-                    f"<li><b>{detail[data]['name']}</b> active with :<ul><li>Iterations max pro run: {detail[data]['iterationMax']}</li><li>Hashtags: {str(detail[data]['tags'])[1:-1]}.</li><li>{detail[data]['usernameLeft']} users remaining.</li></li>"
+            if detail[data]["alive"]:
+                messageBody = (
+                    messageBody
+                    + f"<li><b>{detail[data]['name']}</b> active with :<ul><li>Iterations max pro run: {detail[data]['iterationMax']}</li><li>Hashtags: {str(detail[data]['tags'])[1:-1]}.</li><li>{detail[data]['usernameLeft']} users remaining.</li></li>"
+                )
             else:
-                messageBody = messageBody + \
-                    f"<li>Deactivated! <b>{detail[data]['name']}</b><ul><li>Iterations max pro run: {detail[data]['iterationMax']}</li><li>Hashtags: {str(detail[data]['tags'])[1:-1]}.</li><li>{detail[data]['usernameLeft']} users remaining.</li></li>"
+                messageBody = (
+                    messageBody
+                    + f"<li>Deactivated! <b>{detail[data]['name']}</b><ul><li>Iterations max pro run: {detail[data]['iterationMax']}</li><li>Hashtags: {str(detail[data]['tags'])[1:-1]}.</li><li>{detail[data]['usernameLeft']} users remaining.</li></li>"
+                )
         messageBody = messageBody + "</ol>"
     elif mailType == 4:
         messageSubject = f"[Warning] Add some more user to account {detail}"
@@ -53,19 +65,23 @@ def sendMail(mailType, detail, iteration, runTime):
         messageBody = f"The bot is not running today!"
     else:
         messageSubject = "Subject: All hands on deck!"
-        messageBody = f"Something weird is going on in your python script ({formattedDateStamp})."
+        messageBody = (
+            f"Something weird is going on in your python script ({formattedDateStamp})."
+        )
 
     # Make http resquest to Mailman service
     try:
-        receiver_email = config('EMAIL_GMAIL')
-        url = 'https://mailman-cvs.herokuapp.com/api/instabot'
-        data = {'from': 'instabot <info@clementvanstaen.com>',
-                'to': receiver_email,
-                'subject': messageSubject,
-                'body': messageBody,
-                'key': config('MAILMAN_KEY')}
+        receiver_email = config("EMAIL_GMAIL")
+        url = "https://mailman-cvs.herokuapp.com/api/instabot"
+        data = {
+            "from": "instabot <info@clementvanstaen.com>",
+            "to": receiver_email,
+            "subject": messageSubject,
+            "body": messageBody,
+            "key": config("MAILMAN_KEY"),
+        }
 
-        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        headers = {"Content-type": "application/json", "Accept": "text/plain"}
         request = requests.post(url, data=json.dumps(data), headers=headers)
         # print(request.text)
 
